425d9d2170cfd12abf31a116ef67f2ee248db4d9
Romain Thouvenin <romain@wemove.eu>
2017-12-30 21:49:48 +0100

    Add backlink in description and store id of synced card

--- public/js/team-view.js
+++ public/js/team-view.js
@@ -23,41 +23,67 @@ window.TeamView = {
     });
   },
 
-  doSync: function(trello) {
-    var memberId = null;
+  getContext: function(trello) {
     return Promise.all([
       trello.member('id'),
       trello.organization('id')
-    ])
+    ]);
+  },
+
+  getSyncData: function(trello, member, team) {
+    var memberCards = new Promise(function(resolve, reject) {
+      Trello.get('/members/' + member.id + '/cards', resolve, reject);
+    });
+    var teamBoards = new Promise(function(resolve, reject) {
+      Trello.get('/organizations/' + team.id + '/boards', resolve, reject);
+    });
+    return Promise.all([
+      memberCards,
+      teamBoards,
+      trello.get('member', 'shared', 'teamview_list')
+    ]);
+  },
+
+  createCards: function(list, cardsToCreate) {
+    return Promise.all(cardsToCreate.map(function(card) {
+      return new Promise(function(resolve, reject) {
+        Trello.post('/cards',
+          { 
+            idList: list.id, 
+            name: card.name,
+            desc: "Created from " + card.url
+          },
+          resolve, 
+          reject
+        );
+      });
+    }));
+  },
+
+  storeSyncedIds: function(trello, newCards, cardsToCreate) {
+    return Promise.all(newCards.map(function(card, c) {
+      return trello.set(card.id, 'shared', 'teamview_syncedId', cardsToCreate[c].id);
+    }));
+  },
+
+  doSync: function(trello) {
+    var cardsToCreate = null;
+    return TeamView.getContext(trello)
     .then(function(values) {
       var member = values[0],
           team = values[1];
-      var memberCards = new Promise(function(resolve, reject) {
-        Trello.get('/members/' + member.id + '/cards', resolve, reject);
-      });
-      var teamBoards = new Promise(function(resolve, reject) {
-        Trello.get('/organizations/' + team.id + '/boards', resolve, reject);
-      });
-      return Promise.all([
-        memberCards,
-        teamBoards,
-        trello.get('member', 'shared', 'teamview_list')
-      ]);
+      return TeamView.getSyncData(trello, member, team);
     })
     .then(function (values) {
       var cards = values[0],
           boards = values[1],
           list = values[2];
       var boardIds = boards.map(function(b) { return b.id; });
-      var teamCards = cards.filter(function(c) { return boardIds.includes(c.idBoard); });
-      return Promise.all(teamCards.map(function(card) {
-        return new Promise(function(resolve, reject) {
-          Trello.post('/cards',
-            { idList: list.id, name: card.name },
-            resolve, reject
-          );
-        });
-      }));
+      cardsToCreate = cards.filter(function(c) { return boardIds.includes(c.idBoard); });
+      return TeamView.createCards(list, cardsToCreate);
+    })
+    .then(function(newCards) {
+      return TeamView.storeSyncedIds(trello, newCards, cardsToCreate);
     })
     .catch(function() {
       console.error(arguments);
